79af48dfe683ce21da70870ff6a1898d81274a93
Emily M <30880308+e0marina@users.noreply.github.com>
2020-01-22 19:57:23 -0800

    buttons hide and show pages

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,16 @@
-//{/* <script>
-// //$(document).ready(function(){
-//     $("#hide").click(function(){
-//       $("p").hide();
-//     });
-//     $("#show").click(function(){
-//       $("p").show();
-//     });
-//   });
-// </script> */}
+$(document).ready(function() {
+  //on page load, triviaPage and Score-Page are hidden
+  $(".triviaPage").hide();
+  $("#score-page").hide();
+  //when start button clicked, triviaPage revealed, and start button hidden
+  $("#start").click(function() {
+    $(".triviaPage").show();
+    $("#start").hide();
+  });
+  //when done button clicked, triviaPage hidden and score-page revealed
+  $("#done-button").click(function() {
+    $(".triviaPage").hide();
+    $("#done-button").hide();
+    $("#score-page").show();
+  });
+});
